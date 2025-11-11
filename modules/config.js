28ed605dd5c08a92a02d83008162bced021a813b
xtringx9 <xtringx9@outlook.com>
2025-11-11 11:57:48 +0800

    拆分模块

---
+++ modules/config.js
@@ -0,0 +1,11 @@
+// 配置信息模块
+
+// 扩展基本信息
+export const extensionName = "ST-Continuity-Core";
+export const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
+
+// 默认设置，包含全局开关
+export const defaultSettings = {
+    enabled: true, // 全局开关默认开启
+    backendUrl: "http://192.168.0.119:8888/simple-process" // 后端服务器地址
+};
