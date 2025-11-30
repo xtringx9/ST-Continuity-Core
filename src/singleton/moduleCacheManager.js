15e3227d448d1559e78244d9cd59cd12b9b9efd9
xtringx9 <xtringx9@outlook.com>
2025-11-30 22:53:39 +0800

    feat: 添加模块缓存管理器和样式组合器增强功能
    添加 moduleCacheManager 单例模块用于管理模块缓存
    在 mergeModulesByOrder 中增加 raw 字段存储原始数据
    增强 styleCombiner 功能，支持增量更新时的样式处理和时间线条目样式替换

---
+++ src/singleton/moduleCacheManager.js
@@ -0,0 +1,9 @@
+import { getContext, extension_settings, saveSettingsDebounced, infoLog, errorLog, debugLog } from "../index.js";
+class ModuleCacheManager {
+    constructor() {
+    }
+}
+
+// 创建单例实例
+const moduleCacheManager = new ModuleCacheManager();
+export default moduleCacheManager;
